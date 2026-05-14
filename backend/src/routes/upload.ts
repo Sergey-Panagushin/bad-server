55aff147dd618f93a3b69dafc152efca08e84065
Sergey Panagushin <faceortable@gmail.com>
2026-05-14 22:28:47 +0300

    салфетка проверка 8

--- backend/src/routes/upload.ts
+++ backend/src/routes/upload.ts
@@ -1,9 +1,19 @@
-import { Router } from 'express'
+import { Router, Request, Response, NextFunction } from 'express'
+import multer from 'multer'
 import { uploadFile } from '../controllers/upload'
 import fileMiddleware from '../middlewares/file'
 import auth from '../middlewares/auth'
+import BadRequestError from '../errors/bad-request-error'
 
 const uploadRouter = Router()
-uploadRouter.post('/', auth, fileMiddleware, uploadFile)
+
+uploadRouter.post('/', auth, (req: Request, res: Response, next: NextFunction) => {
+    fileMiddleware(req, res, (err) => {
+        if (err instanceof multer.MulterError || err) {
+            return next(new BadRequestError(err.message))
+        }
+        next()
+    })
+}, uploadFile)
 
 export default uploadRouter
